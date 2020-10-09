0567f403cdfed6ae03db8a2aaf0a6c9fa42c5858
Erick Silva <ericksilvablue@gmail.com>
2020-10-09 15:25:48 -0300

    The new v-model

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,28 @@
+<template>
+<input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+>
+</template>
+
+<script>
+export default {
+    name: 'BaseInput',
+    props: {
+        modelValue: {
+            type: [String, Number],
+            default: ''
+        }
+    }
+}
+</script>
+
+<style>
+input {
+    background: darkslateblue;
+    color: white;
+    font-weight: 600;
+    padding: 1rem 1.75rem;
+    border-style: solid;
+}
+</style>
