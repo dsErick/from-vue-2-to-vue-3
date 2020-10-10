121b633d779beb5a31fe5c74d81ec4b1322fe0ce
Erick Silva <ericksilvablue@gmail.com>
2020-10-10 18:38:26 -0300

    Multi root components

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,28 +1,29 @@
 <template>
-<!-- <div :class="$attrs.class"> -->
-<div>
     <label>{{ label }}</label><br>
     <input
         v-bind="{
-            // ...$attrs,
-            type: $attrs.type,
-            onBlur: $attrs.onBlur,
+            ...$attrs,
+            // type: $attrs.type,
             
+            // onBlur: $attrs.onBlur,
+            onBlur: blurEvent,
             onClick: clickEvent,
 
             // Cleaner to look at and make sure input event is not overwriten
             onInput: e => $emit('update:modelValue', e.target.value)
-            // @input=$emit('update:modelValue', $event.target.value)
         }"
         :value="modelValue"
     >
-</div>
 </template>
 
 <script>
 export default {
     name: 'BaseInput',
-    inheritAttrs: false,    // The parent div will not contain the attributes passed on parent component
+    // inheritAttrs: false,    // The parent div will not contain the attributes passed on parent component
+    emits: {
+        blur: null,
+        'update:modelValue': null
+    },
     props: {
         modelValue: {
             type: [String, Number],
@@ -33,14 +34,16 @@ export default {
             default: null
         }
     },
-    setup(props, { attrs }) {
+    setup(props, { attrs, emit }) {
         console.log('Props', Object.entries(props)) // modelValue: '', label: 'Email'
-        console.log('Attrs', Object.entries(attrs)) // class: 'bg-red', type: 'email', onBlur: f, onUpdate:modelValue: f
+        console.log('Attrs', Object.entries(attrs)) // class: 'bg-red', type: 'email'
 
         const clickEvent = e => console.log(e)
+        const blurEvent = () => emit('blur')
 
         return {
-            clickEvent
+            clickEvent,
+            blurEvent
         }
     }
 }
